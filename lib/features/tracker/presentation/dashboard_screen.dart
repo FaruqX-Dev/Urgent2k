89244a785a4189ed9107afc59188fc48cb8d21af
FaruqX-Dev <faruqbello0303@gmail.com>
2026-01-21 14:28:40 +0100

    ADDED LATEST TRANSACTION SECTION

--- lib/features/tracker/presentation/dashboard_screen.dart
+++ lib/features/tracker/presentation/dashboard_screen.dart
@@ -18,85 +18,112 @@ class _DashboardScreenState extends ConsumerState<DashboardScreen> {
   Widget build(BuildContext context) {
     final textTheme = AppTextTheme.darkTextTheme;
     String currencyFormat = '#';
-    var initialamount = 0;
+    double initialamount = 0.00;
     return SafeArea(
       bottom: true,
       child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            children: [
-              SizedBox(height: 20),
-              Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Welcome back,', style: textTheme.bodyMedium),
-                      Text('Dashboard', style: textTheme.displayLarge),
-                    ],
-                  ),
-                ],
-              ),
-              Card(
-                surfaceTintColor: Colors.black,
-                color: Colors.blue.shade900,
-                elevation: 10,
-
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Total Amount Spent', style: textTheme.titleMedium),
-                      const SizedBox(height: 10),
-                      Text(
-                        '$currencyFormat $initialamount ',
-                        style: textTheme.displayLarge,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          FloatingActionButton(
-                            onPressed: () {},
-                            backgroundColor: Colors.blue.shade700,
-                            child: const Icon(
-                              Icons.add,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 20,),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Spending Overview', style: textTheme.titleMedium),
-                  )
-                ],
-              ),
-              SizedBox(height: 20,),
-              MiniTabCard(),
-              SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                SizedBox(height: 20),
+                Row(
                   children: [
-                    Text(
-                      'latest Transactions', style: textTheme.titleMedium,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Welcome back,', style: textTheme.bodyMedium),
+                        Text('Dashboard', style: textTheme.displayLarge),
+                      ],
                     ),
                   ],
                 ),
-              )
+                Card(
+                  surfaceTintColor: Colors.black,
+                  color: Colors.blue.shade900,
+                  elevation: 10,
+          
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Total Amount Spent', style: textTheme.titleMedium),
+                        const SizedBox(height: 10),
+                        Text(
+                          '$currencyFormat $initialamount ',
+                          style: textTheme.displayLarge,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            FloatingActionButton(
+                              onPressed: () {},
+                              backgroundColor: Colors.blue.shade700,
+                              child: const Icon(
+                                Icons.add,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Spending Overview', style: textTheme.titleMedium),
+                    )
+                  ],
+                ),
+                SizedBox(height: 20,),
+                MiniTabCard(),
+                SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Latest Transactions', style: textTheme.titleMedium,
+                      ),
+                    ],
+                  ),
+                ),
+                ListView.builder(itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
 
-            ],
+                          leading: CircleAvatar(
+                            radius: 25,
+                            backgroundColor: Colors.blue.shade100,
+                            child: Icon(Icons.shopping_cart, color: Colors.blue.shade700,),
+                          ),
+                          title: Text('Grocery Shopping', style: textTheme.bodyMedium,),
+                          subtitle: Text(DateFormat.yMMMd().format(DateTime.now()), style: textTheme.labelSmall,),
+                          trailing: Text('- \#45.00', style: textTheme.bodyMedium?.copyWith(color: Colors.red),),
+                  tileColor: Colors.grey.shade900,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+
+                        ),
+              ),
+
+                    ],
+                  );
+                }, itemCount: 5, shrinkWrap: true, physics: NeverScrollableScrollPhysics(),
+                )],
+            ),
           ),
         ),
       ),
